ee5a598b1deae57e31414ce4e014c624ce945a55
Jason Cochran <jlcochran2013@gmail.com>
2023-09-09 12:19:50 -0500

    .

--- src/app/page.js
+++ src/app/page.js
@@ -199,18 +199,6 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <div className="bg-white py-10 px-6">
-          <h2 className="text-black text-center text-2xl font-bold mb-4 mt-6">
-            Our Technology Stack
-          </h2>
-          <Image
-            src="/logos.png"
-            alt="html5, css3, javascript, typescript, react.js, next.js, node.js, postgresql, mongodb, graphql, json, mysql, git, github"
-            width={1000}
-            height={200}
-            className="mx-auto"
-          />
-        </div>
         <section className="relative text-black flex flex-col w-full py-10 items-center align-middle bg-white">
           <h2 className="text-left text-2xl font-bold mb-4">Our Services</h2>
           <div className="flex flex-wrap justify-center">
@@ -296,6 +284,18 @@ export default function Home() {
               </div>
             </div>
           </div>
+        </section>
+        <section className="bg-white pt-2 pb-10 px-6">
+          <h2 className="text-black text-center text-2xl font-bold mb-4 mt-6">
+            Experts on the Latest Technologies
+          </h2>
+          <Image
+            src="/logos.png"
+            alt="html5, css3, javascript, typescript, react.js, next.js, node.js, postgresql, mongodb, graphql, json, mysql, git, github"
+            width={1000}
+            height={200}
+            className="mx-auto"
+          />
         </section>
         <section
           id="contact"
