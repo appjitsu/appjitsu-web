10acb1cef265780c17a01a902240aef051b8e72c
Jason Cochran <jlcochran2013@gmail.com>
2023-09-08 20:13:17 -0500

    .

--- src/app/api/send-mail/route.js
+++ src/app/api/send-mail/route.js
@@ -1,32 +1,45 @@
 const nodemailer = require("nodemailer");
 
 export async function POST(req) {
-  console.log(process.env.EMAIL_ADDRESS, process.env.EMAIL_PASSWORD);
-  const transporter = nodemailer.createTransport({
-    host: "smtp.forwardemail.net",
-    port: 465,
-    secure: true,
-    auth: {
-      user: process.env.EMAIL_ADDRESS,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
+  try {
+    const transporter = nodemailer.createTransport({
+      host: "smtp.forwardemail.net",
+      port: 465,
+      secure: true,
+      auth: {
+        user: process.env.EMAIL_ADDRESS,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
 
-  const { name, email, message } = await req.json();
-  console.log(name, email, message);
-  const info = await transporter.sendMail({
-    from: "no-reply@appjitsu.net",
-    to: '"Jason Cochran - AppJitsu" <jason@appjitsu.net>',
-    subject: "Website Feedback",
-    text: message,
-  });
+    const { name, email, message } = await req.json();
+    console.log(name, email, message);
+    const info = await transporter.sendMail({
+      from: "no-reply@appjitsu.net",
+      to: '"Jason Cochran - AppJitsu" <jason@appjitsu.net>',
+      subject: "Website Feedback",
+      text: message,
+    });
 
-  await transporter.sendMail({
-    from: "no-reply@appjitsu.net",
-    to: `"${name}" <${email}>`,
-    subject: "Website Feedback",
-    text: "Thank you for your reaching out to us! We will get back to you as soon as possible.",
-  });
+    await transporter.sendMail({
+      from: "no-reply@appjitsu.net",
+      to: `"${name}" <${email}>`,
+      subject: "Website Feedback",
+      text: "Thank you for your reaching out to us! We will get back to you as soon as possible.",
+    });
 
-  console.log("Message sent: %s", info.messageId);
+    console.log("Message sent: %s", info.messageId);
+
+    return new Response(
+      JSON.stringify({ message: "Message sent successfully!" }),
+      {
+        status: 200,
+      }
+    );
+  } catch (error) {
+    console.error(error);
+    return new Response(JSON.stringify({ error: error.message }), {
+      status: 500,
+    });
+  }
 }
